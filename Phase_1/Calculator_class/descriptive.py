6e6ae4775c4ed40ef1eafa100de484ae7cb21f26
Ivan <ivan.zakharchuk@gmail.com>
2020-12-16 23:04:46 -0500

    Updates

--- Phase_1/Calculator_class/descriptive.py
+++ Phase_1/Calculator_class/descriptive.py
@@ -9,4 +9,57 @@ import math
 
 
 class Calculator:
-    pass        
+    def __init__(self,data):
+        self.data = sorted(data)
+        self.data = self._calc_stats()
+    def _calc_mean(self):
+        return self.total/self.length
+    
+    def _calc_median(self):
+        n = self.length
+        if n % 2 == 1:
+            return self.data[(n-1)//2]
+        else:
+            return (self.data[n//2-1]+self.data[n//2])/2
+
+    def _calc_variance(self):
+        sum_value = 0
+        for item in self.data:
+            sum_value += (item - (self.mean))**2
+        return sum_value/(self.length-1)
+
+    def _calc_stddev(self):
+        return math.sqrt(self.variance)    
+
+    def _calc_stats(self):
+        self.length = len(self.data)
+        self.total = sum(self.data)
+        self.mean = self._calc_mean()
+        self.median = self._calc_median()
+        self.variance = self._calc_variance()
+        self.stand_dev = self._calc_stddev()
+
+
+    def add_data(self, new_data):
+        """Takes a list of data, adds all of those values
+            from the data set of the object,
+            and recalculates the descriptive statistics.
+        """
+        self.data.extend(new_data)
+        self.data = sorted(self.data)
+        self._calc_stats()
+        return
+
+    def remove_data(self, new_data):
+        """Takes a list of data, removes all of those values
+            from the data set of the object,
+            and recalculates the descriptive statistics.
+        """
+        self.data = [x for x in self.data if x not in new_data]
+        self._calc_stats()
+        return
+#import pdb;pdb.set_trace()
+# our_calc = Calculator([1,2,3,4,5])
+
+
+# pytest -q test_calculator.py 
